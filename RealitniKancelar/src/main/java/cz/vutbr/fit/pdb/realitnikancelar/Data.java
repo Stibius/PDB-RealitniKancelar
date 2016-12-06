7939a7a46ba1e62521b732d1862b5e1ce2874547
Jiri Dostal <xdosta40@stud.fit.vutbr.cz>
2016-12-06 20:44:32 +0100

    Refaktorizace Data.java. Novy DatabaseHelper

--- RealitniKancelar/src/main/java/cz/vutbr/fit/pdb/realitnikancelar/Data.java
+++ RealitniKancelar/src/main/java/cz/vutbr/fit/pdb/realitnikancelar/Data.java
@@ -17,7 +17,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import oracle.spatial.geometry.JGeometry;
-import oracle.sql.STRUCT;
 
 /**
  * @author Honza
@@ -70,7 +69,11 @@ public class Data {
         ObjectInfo info = null;
 
         try (Statement stmt = ConnectDialog.conn.createStatement()) {
-            ResultSet res = stmt.executeQuery("SELECT * FROM OBJEKTY LEFT OUTER JOIN MAJITELE_OBJEKTY ON objekty.ID=majitele_objekty.IDOBJEKTU LEFT OUTER JOIN MAJITELE ON majitele_objekty.IDMAJITELE=majitele.id_majitele");
+            ResultSet res = stmt.executeQuery("SELECT * FROM OBJEKTY " +
+                    "LEFT OUTER JOIN MAJITELE_OBJEKTY ON objekty.ID=majitele_objekty" +
+                    ".IDOBJEKTU " +
+                    "LEFT OUTER JOIN MAJITELE ON majitele_objekty.IDMAJITELE=majitele" +
+                    ".id_majitele");
             while (res.next()) {
                 //pokud nemame zadne info, ObjectInfo neexistuje, tudiz ani objekt
                 if (!ObjectInfo.ids.contains(res.getInt("id"))) {
@@ -211,204 +214,89 @@ public class Data {
     public static void saveData() throws InvalidObjectException, SQLException {
         //Vytvorime testovaci sektor, bacha, smaze vsechny ostatni
         Sektor.testovaciSektor();
-        Shape current;
-        ObjectInfo currentInfo;
-        Connection conn = ConnectDialog.conn;
 
+        /* MAJITELE */
+        saveOwners();
+        /* KONEC MAJITELU */
+
+        /* OBJEKTY */
+        Map<ObjectInfo, Shape> objects = mergeShapes();
+
+        saveObjects(objects);
+        /* KONEC OBJEKTU */
+
+        dataSaved();
+    }
+
+    private static void saveOwners() {
+        Owner currentOwner;
         for (int i = 0; i < owners.size(); i++) {
-            Owner currentOwner = owners.get(i);
+            currentOwner = owners.get(i);
+
+            /* neni novy ani modifikovany ani smazany, preskocit */
             if (!currentOwner.modifiedOwner &&
                     !currentOwner.newOwner &&
                     !currentOwner.deletedOwner) {
-                continue; //neni novy ani modifikovany ani smazany, preskocit
+                continue;
             }
 
+            /* byl smazany v aplikaci, je treba ho smazat z DB */
             if (currentOwner.deletedOwner) {
-                //byl smazany v aplikaci, je treba ho smazat z DB
-                if (currentOwner.deletedOwner) {
-                    String query = "DELETE FROM majitele WHERE id_majitele='" + currentOwner
-                            .id +
-                            "'";
-                    try {
-                        Statement stmt = conn.createStatement();
-                        stmt.executeQuery(query);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    continue;
-                }
+                DatabaseHelper.deleteOwner(currentOwner);
+                continue;
             }
-            //je modifikovany
+
+            /* majitel je modifikovany */
             if (currentOwner.modifiedOwner) {
-                try (PreparedStatement stmt = conn.prepareStatement("UPDATE majitele SET " +
-                        "jmeno = ?, adresa = ? WHERE id_majitele = ?")) {
-                    stmt.setString(1, currentOwner.jmeno);
-                    stmt.setString(2, currentOwner.adresa);
-                    stmt.setInt(3, currentOwner.id);
-                    stmt.execute();
-                    continue;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                DatabaseHelper.modifyOwner(currentOwner);
+                continue;
             }
-            //je novy
+            /* majitel je novy */
             if (currentOwner.newOwner) {
-                try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO " +
-                                "majitele (id_majitele, jmeno, adresa) VALUES (?, ?, ?)")) {
-                    stmt.setInt(1, currentOwner.id);
-                    stmt.setString(2, currentOwner.jmeno);
-                    stmt.setString(3, currentOwner.adresa);
-                    stmt.execute();
-                    continue;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                DatabaseHelper.newOwner(currentOwner);
+                continue;
             }
-
         }
+    }
 
-        Map<ObjectInfo, Shape> objects = mergeShapes();
-
+    private static void saveObjects(Map<ObjectInfo, Shape> objects) throws SQLException {
+        ObjectInfo currentInfo;
+        Shape current;
         for (Map.Entry<ObjectInfo, Shape> entry : objects.entrySet()) {
             currentInfo = entry.getKey();
             current = entry.getValue();
 
+            /* neni novy ani modifikovany ani smazany, preskocit */
             if (!currentInfo.modifiedInfo &&
                     !currentInfo.modifiedGeometry &&
                     !currentInfo.modifiedImage &&
                     !currentInfo.newObject &&
                     !currentInfo.deletedObject) {
-                continue; //neni novy ani modifikovany ani smazany, preskocit
+                continue;
             }
 
             /* Pokud je smazany, smazeme ho i z DB */
             if (currentInfo.deletedObject) {
-                String query = "DELETE FROM objekty WHERE id='" + currentInfo.id + "'";
-                try {
-                    Statement stmt = conn.createStatement();
-                    stmt.executeQuery(query);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                DatabaseHelper.deleteObject(currentInfo);
                 continue;
             }
 
             /* Pokud je novy, udelame plny insert */
             if (currentInfo.newObject) {
-                JGeometry jGeo = ShapeHelper.shape2jGeometry(current);
-
-                try {
-                    /* Nejdriv naplnime tabulku 'objekty' */
-                    PreparedStatement stmt = conn.prepareStatement("INSERT INTO objekty (id," +
-                            " nazev,typ,editable,popis,sektor,geometrie," +
-                            "existenceOd,existenceDo,rekonstrukce) " +
-                            "VALUES (?,?,?,?,?,?,?,?,?,?)" +
-                            "");
-                    STRUCT obj = JGeometry.store(conn, jGeo);
-                    stmt.setInt(1, currentInfo.id);
-                    stmt.setString(2, currentInfo.nazev);
-                    stmt.setString(3, currentInfo.typ);
-                    stmt.setBoolean(4, currentInfo.editable);
-                    stmt.setString(5, currentInfo.popis);
-                    stmt.setInt(6, currentInfo.sektor);
-                    stmt.setObject(7, obj);
-                    stmt.setDate(8, new java.sql.Date(currentInfo.existenceOd.getTime()));
-                    stmt.setDate(9, new java.sql.Date(currentInfo.existenceDo.getTime()));
-                    stmt.setDate(10, new java.sql.Date(currentInfo.rekonstrukce.getTime()));
-                    stmt.execute();
-
-                    /* Ted tabulku 'majitele_objekty' */
-                    stmt = conn.prepareStatement("INSERT INTO majitele_objekty " +
-                            "(IDOBJEKTU,IDMAJITELE,MAJITELOD,MAJITELDO) VALUES (?,?,?,?)");
-
-                    for (int i = 0; i < currentInfo.majitele.size(); i++) {
-                        stmt.setInt(1, currentInfo.id);
-                        stmt.setInt(2, currentInfo.majitele.get(i).id);
-                        stmt.setDate(3, new java.sql.Date(currentInfo.majitelOd.get(i)
-                                .getTime()));
-                        stmt.setDate(4, new java.sql.Date(currentInfo.majitelDo.get(i)
-                                .getTime()));
-                        stmt.addBatch();
-                    }
-                    stmt.executeBatch();
-
-                    /* Ted obrazek */
-                    if (currentInfo.modifiedImage) {
-                        try {
-                            currentInfo.saveFotoToDB();
-                        } catch (IOException ex) {
-                            Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                    continue;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                DatabaseHelper.newObject(current, currentInfo);
+                continue;
             }
 
             /* Je zmenena geometrie, aktualizujeme */
             if (currentInfo.modifiedGeometry) {
-                JGeometry jGeo = ShapeHelper.shape2jGeometry(current);
-                //Nejspis se to musi vsechno vyjmenovat pokud chceme doplnovat pozdeji
-                try (PreparedStatement stmt = conn.prepareStatement("UPDATE objekty SET " +
-                        "geometrie = ? WHERE id = ?")) {
-                    STRUCT obj = JGeometry.store(conn, jGeo);
-                    stmt.setObject(1, obj);
-                    stmt.setInt(2, currentInfo.id);
-
-                    stmt.execute();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                DatabaseHelper.modifyObjectGeometry(current, currentInfo);
             }
 
             /* Modifikovane informace, aktualizujeme */
             if (currentInfo.modifiedInfo) {
-                /* Nejdriv update tabulky 'objekty' */
-                try {
-                    PreparedStatement stmt = conn.prepareStatement("UPDATE objekty " +
-                            "SET nazev=?,typ=?,editable=?,popis=?,sektor=?," +
-                            "existenceOd=?,existenceDo=?,rekonstrukce=?" +
-                            "WHERE id = ?");
-
-                    stmt.setString(1, currentInfo.nazev);
-                    stmt.setString(2, currentInfo.typ);
-                    stmt.setBoolean(3, currentInfo.editable);
-                    stmt.setString(4, currentInfo.popis);
-                    stmt.setInt(5, currentInfo.sektor);
-                    stmt.setDate(6, new java.sql.Date(currentInfo.existenceOd.getTime()));
-                    stmt.setDate(7, new java.sql.Date(currentInfo.existenceDo.getTime()));
-                    stmt.setDate(8, new java.sql.Date(currentInfo.rekonstrukce.getTime()));
-                    stmt.setInt(9, currentInfo.id);
-
-                    stmt.execute();
-
-                    /* Ted tabulka 'majitele_objekty'
-                    Kvuli vsem moznym zmenam v majitelich, obdobich, poradi atd je
-                    nejlepsi udelat full-refresh
-                     */
-                    Statement delStmt = conn.createStatement();
-                    delStmt.executeQuery("DELETE FROM majitele_objekty WHERE idobjektu = " +
-                            "'"+currentInfo.id + "'");
-                    /* Ted je vlozime nazpet */
-                    stmt = conn.prepareStatement("INSERT INTO majitele_objekty " +
-                            "(IDOBJEKTU,IDMAJITELE,MAJITELOD,MAJITELDO) VALUES (?,?,?,?)");
-
-                    for (int i = 0; i < currentInfo.majitele.size(); i++) {
-                        stmt.setInt(1, currentInfo.id);
-                        stmt.setInt(2, currentInfo.majitele.get(i).id);
-                        stmt.setDate(3, new java.sql.Date(currentInfo.majitelOd.get(i)
-                                .getTime()));
-                        stmt.setDate(4, new java.sql.Date(currentInfo.majitelDo.get(i)
-                                .getTime()));
-                        stmt.addBatch();
-                    }
-                    stmt.executeBatch();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                DatabaseHelper.modifyObjectInfo(currentInfo);
             }
-            //modifikace obrázku
+            /* Modifikovany obrazek */
             if (currentInfo.modifiedImage) {
                 try {
                     currentInfo.saveFotoToDB();
@@ -416,10 +304,7 @@ public class Data {
                     Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-
         }
-
-        dataSaved();
     }
 
     public static Map mergeShapes() {
---
+++ RealitniKancelar/src/main/java/cz/vutbr/fit/pdb/realitnikancelar/DatabaseHelper.java
@@ -0,0 +1,215 @@
+package cz.vutbr.fit.pdb.realitnikancelar;
+
+import oracle.spatial.geometry.JGeometry;
+import oracle.sql.STRUCT;
+
+import java.awt.*;
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Helper pro manipulaci s databazi
+ * Created by jiri on 6.12.16.
+ */
+public class DatabaseHelper {
+    private static Connection conn = ConnectDialog.conn;
+
+    /**
+     * Smaze majitele z databaze
+     *
+     * @param currentOwner
+     */
+    public static void deleteOwner(Owner currentOwner) {
+        String query = "DELETE FROM majitele WHERE id_majitele='" + currentOwner.id + "'";
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Upravi majitele v databazi
+     *
+     * @param currentOwner
+     */
+    public static void modifyOwner(Owner currentOwner) {
+        try (PreparedStatement stmt = conn.prepareStatement("UPDATE majitele SET " +
+                "jmeno = ?, adresa = ? WHERE id_majitele = ?")) {
+            stmt.setString(1, currentOwner.jmeno);
+            stmt.setString(2, currentOwner.adresa);
+            stmt.setInt(3, currentOwner.id);
+            stmt.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Prida noveho majitele
+     *
+     * @param currentOwner
+     */
+    public static void newOwner(Owner currentOwner) {
+        try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO " +
+                "majitele (id_majitele, jmeno, adresa) VALUES (?, ?, ?)")) {
+            stmt.setInt(1, currentOwner.id);
+            stmt.setString(2, currentOwner.jmeno);
+            stmt.setString(3, currentOwner.adresa);
+            stmt.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Smaze objekt z databaze
+     *
+     * @param currentInfo
+     */
+    public static void deleteObject(ObjectInfo currentInfo) {
+        String query = "DELETE FROM objekty WHERE id='" + currentInfo.id + "'";
+        try {
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Vlozi kompletne novy objekt
+     * @param current
+     * @param currentInfo
+     */
+    public static void newObject(Shape current, ObjectInfo currentInfo) {
+        JGeometry jGeo = null;
+        try {
+            jGeo = ShapeHelper.shape2jGeometry(current);
+        } catch (InvalidObjectException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            /* Nejdriv naplnime tabulku 'objekty' */
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO objekty (id," +
+                    " nazev,typ,editable,popis,sektor,geometrie," +
+                    "existenceOd,existenceDo,rekonstrukce) " +
+                    "VALUES (?,?,?,?,?,?,?,?,?,?)" +
+                    "");
+            STRUCT obj = JGeometry.store(conn, jGeo);
+            stmt.setInt(1, currentInfo.id);
+            stmt.setString(2, currentInfo.nazev);
+            stmt.setString(3, currentInfo.typ);
+            stmt.setBoolean(4, currentInfo.editable);
+            stmt.setString(5, currentInfo.popis);
+            stmt.setInt(6, currentInfo.sektor);
+            stmt.setObject(7, obj);
+            stmt.setDate(8, new java.sql.Date(currentInfo.existenceOd.getTime()));
+            stmt.setDate(9, new java.sql.Date(currentInfo.existenceDo.getTime()));
+            stmt.setDate(10, new java.sql.Date(currentInfo.rekonstrukce.getTime()));
+            stmt.execute();
+
+            /* Ted tabulku 'majitele_objekty' */
+            stmt = conn.prepareStatement("INSERT INTO majitele_objekty " +
+                    "(IDOBJEKTU,IDMAJITELE,MAJITELOD,MAJITELDO) VALUES (?,?,?,?)");
+
+            for (int i = 0; i < currentInfo.majitele.size(); i++) {
+                stmt.setInt(1, currentInfo.id);
+                stmt.setInt(2, currentInfo.majitele.get(i).id);
+                stmt.setDate(3, new java.sql.Date(currentInfo.majitelOd.get(i)
+                        .getTime()));
+                stmt.setDate(4, new java.sql.Date(currentInfo.majitelDo.get(i)
+                        .getTime()));
+                stmt.addBatch();
+            }
+            stmt.executeBatch();
+
+            /* Ted obrazek */
+            if (currentInfo.modifiedImage) {
+                try {
+                    currentInfo.saveFotoToDB();
+                } catch (IOException ex) {
+                    Logger.getLogger(Data.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void modifyObjectGeometry(Shape current, ObjectInfo currentInfo) {
+        JGeometry jGeo = null;
+        try {
+            jGeo = ShapeHelper.shape2jGeometry(current);
+        } catch (InvalidObjectException e) {
+            e.printStackTrace();
+        }
+        //Nejspis se to musi vsechno vyjmenovat pokud chceme doplnovat pozdeji
+        try (PreparedStatement stmt = conn.prepareStatement("UPDATE objekty SET " +
+                "geometrie = ? WHERE id = ?")) {
+            STRUCT obj = JGeometry.store(conn, jGeo);
+            stmt.setObject(1, obj);
+            stmt.setInt(2, currentInfo.id);
+
+            stmt.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void modifyObjectInfo(ObjectInfo currentInfo) {
+        /* Nejdriv update tabulky 'objekty' */
+        try {
+            PreparedStatement stmt = conn.prepareStatement("UPDATE objekty " +
+                    "SET nazev=?,typ=?,editable=?,popis=?,sektor=?," +
+                    "existenceOd=?,existenceDo=?,rekonstrukce=?" +
+                    "WHERE id = ?");
+
+            stmt.setString(1, currentInfo.nazev);
+            stmt.setString(2, currentInfo.typ);
+            stmt.setBoolean(3, currentInfo.editable);
+            stmt.setString(4, currentInfo.popis);
+            stmt.setInt(5, currentInfo.sektor);
+            stmt.setDate(6, new java.sql.Date(currentInfo.existenceOd.getTime()));
+            stmt.setDate(7, new java.sql.Date(currentInfo.existenceDo.getTime()));
+            stmt.setDate(8, new java.sql.Date(currentInfo.rekonstrukce.getTime()));
+            stmt.setInt(9, currentInfo.id);
+
+            stmt.execute();
+
+            /* Ted tabulka 'majitele_objekty'
+            Kvuli vsem moznym zmenam v majitelich, obdobich, poradi atd je
+            nejlepsi udelat full-refresh
+            */
+            Statement delStmt = conn.createStatement();
+            delStmt.executeQuery("DELETE FROM majitele_objekty WHERE idobjektu = " +
+                    "'" + currentInfo.id + "'");
+                    /* Ted je vlozime nazpet */
+            stmt = conn.prepareStatement("INSERT INTO majitele_objekty " +
+                    "(IDOBJEKTU,IDMAJITELE,MAJITELOD,MAJITELDO) VALUES (?,?,?,?)");
+
+            for (int i = 0; i < currentInfo.majitele.size(); i++) {
+                stmt.setInt(1, currentInfo.id);
+                stmt.setInt(2, currentInfo.majitele.get(i).id);
+                stmt.setDate(3, new java.sql.Date(currentInfo.majitelOd.get(i)
+                        .getTime()));
+                stmt.setDate(4, new java.sql.Date(currentInfo.majitelDo.get(i)
+                        .getTime()));
+                stmt.addBatch();
+            }
+            stmt.executeBatch();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
